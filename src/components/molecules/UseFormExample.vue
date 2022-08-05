6c75570ad092806dfef26b941a1b6bd1f5b0cd47
Максим Тутуков <m.tutukov@kokoc.tech>
2022-08-05 23:19:18 +0300

    feat(master): Init project

--- src/components/molecules/UseFormExample.vue
+++ src/components/molecules/UseFormExample.vue
@@ -1,9 +1,11 @@
 <template>
-  <router-view />
+  <form>
+    <h2>Auth</h2>
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'UseFormExample'
 }
 </script>
--- src/pages/PageMain.vue
+++ src/pages/PageMain.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>main</div>
+  <h1>Vue 3 custom hooks</h1>
+  <UseFormExample />
 </template>
 
 <script>
+import UseFormExample from '@/components/molecules/UseFormExample.vue'
+
 export default {
   name: 'PageMain',
-};
+
+  components: { UseFormExample }
+}
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
